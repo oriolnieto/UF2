7c91db7c80881218cdb4faea5e4c7504ba518078
p1re <oriolnieto2006@gmail.com>
2025-12-08 10:01:43 +0100

    Initial commit

---
+++ src/UF2/cFil.java
@@ -0,0 +1,37 @@
+package UF2;
+
+// alternativa: extends Thread
+public class cFil implements Runnable {
+
+    String aNomFil;
+    int aTemporitzacio;
+
+    public cFil(String pNomFil) {
+        aTemporitzacio = 500;
+        aNomFil = pNomFil;
+    }
+
+    public String gNomFil() {
+        return aNomFil;
+    }
+
+    public void sTemporitzacio(int pTemporitzacio) {
+        aTemporitzacio = pTemporitzacio;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Iniciant execució procés " + aNomFil);
+
+        try {
+            for (int vComptador = 0; vComptador < 10; vComptador++) {
+                Thread.sleep(aTemporitzacio);
+                System.out.println("Despertant aturada " + vComptador + " procés " + aNomFil);
+            }
+        } catch (InterruptedException pExcepcio) {
+            System.out.println("Interrompent execució procés " + aNomFil);
+        }
+
+        System.out.println("Acabant execució procés " + aNomFil);
+    }
+}
---
+++ src/UF2/cFilPrincipal.java
@@ -0,0 +1,38 @@
+package UF2;
+
+import UF2.cFil;
+
+public class cFilPrincipal {
+
+    public static void main(String[] pArguments) {
+
+        System.out.println("Fil principal iniciat.");
+        System.out.println("Fil secundari iniciat.");
+
+        cFil vObjecteFil = new cFil("#1");
+
+        // alternativa: innecessari
+        Thread vFil = new Thread(vObjecteFil);
+
+        // alternativa: vObjecteFil
+        vFil.start();
+
+        System.out.println("Iniciant execució procés principal");
+
+        try {
+            for (int vComptador = 0; vComptador < 10; vComptador++) {
+
+                Thread.sleep(500);
+
+                System.out.println("Despertant aturada "
+                        + vComptador
+                        + " procés principal");
+            }
+
+        } catch (InterruptedException pExcepcio) {
+            System.out.println("Interrompent execució procés principal");
+        }
+
+        System.out.println("Acabant execució procés principal");
+    }
+}
