35d78fe89397ab50478ebe87213a502a3547f4fc
p1re <oriolnieto2006@gmail.com>
2025-12-09 17:11:03 +0100

    #3 Feta (for now)
    fa falta detallar explicació millor...

--- src/UF2/cFilPrincipal.java
+++ src/UF2/cFilPrincipal.java
@@ -4,19 +4,20 @@ import UF2.cFil;
 
 public class cFilPrincipal {
 
-    public static void main(String[] pArguments) {
+    public static void main(String[] pArguments) throws InterruptedException {
 
         System.out.println("Fil principal iniciat.");
         System.out.println("Fil secundari iniciat.");
 
         cFil vObjecteFil = new cFil("#1");
-        vObjecteFil.sTemporitzacio(100);
+        vObjecteFil.sTemporitzacio(1000);
 
         // alternativa: innecessari
         Thread vFil = new Thread(vObjecteFil);
 
         // alternativa: vObjecteFil
         vFil.start();
+        vFil.join();  // ho podriem fer tantejant els milisegons amb el sleep del procés principal (pare), pero apart de que no estaria sincronitzat i imprecis, utilitzem join per basicament dir al procés pare que s'ha de esperar al procés fill a que acabi, li podem passar per parametre milisegons de espera també.
 
         System.out.println("Iniciant execució procés principal");
 
