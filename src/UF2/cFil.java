31cbde1212219c13a8985827029d6b5f3cdac7bf
jan altisent <altisentbrescojan@alumnes.ilerna.com>
2025-12-11 15:55:02 +0100

    comentari global cfil

--- src/UF2/cFil.java
+++ src/UF2/cFil.java
@@ -1,5 +1,19 @@
 package UF2;
 
+/* la classe Cfil compta amb diferents funcionalitats.
+ *  - Instanciar objectes a partir dels que es puguin crear fils d'execució paral·lela.
+ *  - La classe compta amb un constructor que accepta com a paràmetre un nom pel procès creat.
+ *  - També compta amb un getter on realitza una consulta del nombre del procès creat.
+ *  - També compta amb un setter que modifica l'atribut de temporització de la classe.
+ *  - El métode run() sobrecàrrega la funció que representa el procediment main.
+ *
+ *  Les dues primeres tasques de la pràctica es treballen en aquesta classe.
+ *  1.1 Necessàriament modificar la temporització del procès fill, on acabi la seva execució
+ *      bastant més abans del seu procès pare. Mitjançant un métode concret.
+ *
+ *  1.2 Tornar a modificar la temporització del procès fill, però aquest cop es vol finalitzar
+ *      la seva execució posteriorment a la del pare. Mitjançant un métode concret.      */
+
 // alternativa: extends Thread
 public class cFil implements Runnable {
 
